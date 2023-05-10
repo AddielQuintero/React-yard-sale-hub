2c20973f6b81ec78c9035e2067c3b99056d2a16d
atamayo <atamayoquintero@gmail.com>
2023-05-10 11:35:50 -0500

    feat: add Headers components(AuthButton, Cart, CustomLinkList, NavBar, OrdersCart, UserMenu )

---
+++ src/components/Header/NavBar.tsx
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import logo from '../../assets/logo_yard_sale.svg'
+import { Cart, CustomLinkList, UserMenu } from './'
+import { NavLink } from 'react-router-dom'
+import { NavigateProps } from '../../types'
+
+export const NavBar = ({ navigation }: { navigation: NavigateProps }) => {
+  return (
+    <div>
+      <Navbar collapseOnSelect bg="light" expand="md" className="custom--nav mb-3 border-md-bottom  ">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="canvasNavbar" className="border-0 focus-ring focus-ring-light py-0" />
+
+          <NavLink to="/">
+            <img src={logo} alt="logo" className="" />
+          </NavLink>
+          <Navbar.Offcanvas id="canvasNavbar"  placement="start" aria-labelledby="canvasNavbar">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="canvasNavbar">
+                <Nav.Link as={NavLink} to="/" eventKey="1">
+                  <img src={logo} alt="logo" className="" />
+                </Nav.Link>
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-start align-items-md-center flex-grow-1 px-md-5">
+                <CustomLinkList navigation={navigation} className="link__list text-600" />
+              </Nav>
+              <div className="border-top d-md-none my-md-0 my-5"></div>
+              <UserMenu />
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+
+          <Cart />
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
---
+++ src/components/Header/UserMenu.tsx
@@ -0,0 +1,69 @@
+import { NavLink } from 'react-router-dom'
+import { AuthService } from '../../services'
+import Nav from 'react-bootstrap/Nav'
+import { useState } from 'react'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const menu = [
+  { name: 'My account', to: '/profile', private: true },
+  { name: 'My orders', to: '/orders', private: true },
+]
+
+export const UserMenu = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const { to, linkText, handleClick, icon } = AuthService().AuthConfigItem
+
+  const handleMenu = () => {
+    handleClick?.()
+    // handleClose?.()
+    setIsAuthenticated(!isAuthenticated)
+  }
+
+  return (
+    <div className="menu  d-flex flex-column justify-content-center">
+      {!isAuthenticated ? (
+        <Nav>
+          <NavDropdown
+            id="down-centered"
+            title="platzi@example.com"
+            drop="down-centered"
+            menuVariant="light"
+            className="NavDropdown"
+          >
+            {menu.map((link) => (
+              <NavDropdown.Item
+                as={NavLink}
+                key={link.name}
+                to={link.to}
+                eventKey={link.name}
+                className="text-md-end px-0 px-md-4"
+              >
+                {link.name}
+              </NavDropdown.Item>
+            ))}
+            <NavDropdown.Divider className='mx-md-4'/>
+            <NavDropdown.Item
+              as={NavLink}
+              to={to}
+              eventKey="1"
+              className="btn bg-transparent btn-light border-0 d-flex justify-content-md-end align-items-center gap-1 px-0 px-md-4"
+              onClick={handleMenu}
+            >
+              {linkText} {icon}
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      ) : (
+        <Nav.Link
+          as={NavLink}
+          to={to}
+          eventKey="1"
+          className="btn bg-transparent btn-light border-0 d-flex justify-content-center align-items-center gap-1"
+          onClick={handleMenu}
+        >
+          {linkText} {icon}
+        </Nav.Link>
+      )}
+    </div>
+  )
+}
