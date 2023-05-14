fc61f2fdc74d98ffbfbcb42bb877f2798999ed25
atamayo <atamayoquintero@gmail.com>
2023-05-14 18:51:02 -0500

    feat: add CustomToast component

---
+++ src/components/common/Toast.tsx
@@ -0,0 +1,37 @@
+import { Row, Col } from 'react-bootstrap'
+import Toast from 'react-bootstrap/Toast'
+import ToastContainer from 'react-bootstrap/ToastContainer'
+import { ToastProps } from '@types'
+
+export const CustomToast = ({ counter, setCounter, show, handleClose, text }: ToastProps) => {
+  const onClose = () => {
+    handleClose(false)
+    setCounter(0)
+  }
+
+  return (
+    <Row>
+      <Col xs={6}>
+        <ToastContainer className="p-3 position-fixed" position="top-end">
+          {Array.from({ length: counter }, (_, index) => (
+            <Toast
+              key={index}
+              className="w-100 border-0"
+              onClose={() => onClose()}
+              show={show}
+              delay={2000}
+              autohide
+            >
+              <Toast.Header
+                closeVariant="white"
+                className="bg-green-500 text-white py-3 border-bottom-0 rounded-1"
+              >
+                <strong className="me-auto">{text}</strong>
+              </Toast.Header>
+            </Toast>
+          ))}
+        </ToastContainer>
+      </Col>
+    </Row>
+  )
+}
