ab1bb90df7fa9753351b888e892acd74506dac99
atamayo <atamayoquintero@gmail.com>
2023-05-18 16:25:00 -0500

    feat: add filter Component by price and categories

--- src/components/Header/Navigation/index.ts
+++ src/components/Header/Navigation/index.ts
@@ -1,2 +1,3 @@
+export * from './Filter';
 export * from './LinkList';
 export * from './NavBar';
--- src/services/Product.service.tsx
+++ src/services/Product.service.tsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { TProduct } from '@types'
+import { DefaultFilters, TFilters, TProduct } from '@types'
 
 // const BASE_URL = 'http://localhost:3000/dataStore'
 const BASE_URL = 'https://json-server-router-dom.herokuapp.com/dataStore'
 
 export const ProductService = () => {
   const [products, setProducts] = useState<TProduct[]>([])
-  // console.log(products)
+  const [filters, setFilters] = useState<TFilters>(DefaultFilters)
+  // console.log(filters)
 
   useEffect(() => {
     axios
-      .get(` ${BASE_URL}?_page=1&_limit=8`)
+      .get(` ${BASE_URL}?_page=1&_limit=20`)
       .then((response) => setProducts(response.data))
       .catch((error) => {
         console.error('Error fetching products:', error)
       })
-    console.log('get into useEffect')
+    // console.log('get api')
   }, [setProducts])
 
-  return { products }
+  const productFilters = (products: TProduct[]) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === 'All' || filters.category === product.category.name)
+      )
+    })
+  }
+
+  const productFiltered = productFilters(products)
+
+  return { products, productFiltered, filters, setFilters }
 }
