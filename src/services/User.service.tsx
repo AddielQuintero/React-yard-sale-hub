037da1473754509f7fd8f44e779992e8b6888716
atamayo <atamayoquintero@gmail.com>
2023-06-01 09:27:49 -0500

    feat: add environment configuration for API base URL and update API call in service

--- src/services/User.service.tsx
+++ src/services/User.service.tsx
@@ -1,14 +1,12 @@
 // import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { TUser } from '@types'
-
-// const BASE_URL = 'http://localhost:3000/users'
-const BASE_URL = 'https://json-server-router-dom.herokuapp.com/users'
+import { CONFIG } from '@config'
 
 class UserService {
   async getUser(userName: string): Promise<TUser> {
     try {
-      const { data } = await axios.get<TUser[]>(`${BASE_URL}?userName=${userName}`)
+      const { data } = await axios.get<TUser[]>(`${CONFIG.API_BASE}/users?userName=${userName}`)
       return data[0]
     } catch (error) {
       console.error(`UserError: ${error}`)
